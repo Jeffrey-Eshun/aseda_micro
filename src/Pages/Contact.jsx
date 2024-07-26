6aac8c68f389ffd6ff593d2a20187a948b443ee4
Jeffrey Jojo Eshun <jefferyeshun97@gmail.com>
2024-07-26 23:15:28 +0000

    responsive

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -12,14 +12,14 @@ function Contact() {
 
       <div className="business-con">
         <h1>BUSINESS TO BUSINESS</h1>
-        <p>We are specialists in both economics and information technologies and we apply our full range of talent to <br/>creating the perfect solution for each client’s needs</p>
+        <p>We are specialists in both economics and information technologies and we apply our full range of talent to creating the perfect solution for each client’s needs</p>
       </div>
 
       <div className="build-img">
         <div className="build-1">
           <h5>contact us</h5>
           <h1>Get in touch with us</h1>
-          <p>If you’d like to know more about how we can help you plan,<br/> schedule, and manage your time more effectively on every<br/> development project, simply fill out the form to get in touch<br/> with one of our experts.</p>
+          <p>If you’d like to know more about how we can help you plan, schedule, and manage your time more effectively on every development project, simply fill out the form to get in touch with one of our experts.</p>
         </div>
         <div className="build-2">
           <form action="active">
